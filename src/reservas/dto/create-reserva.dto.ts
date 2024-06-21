b47b9a6efae0bc0fd2d3210282f7bafb821c6218
Misttes <hernxnpereyra@gmail.com>
2024-06-21 14:00:53 -0500

    feat: Add data validation using class-validator and class-transformer
    - Installed class-validator and class-transformer libraries.
    - Update DTOs with validation rules.
    - Configured global validation pipe in main.ts.

--- src/reservas/dto/create-reserva.dto.ts
+++ src/reservas/dto/create-reserva.dto.ts
@@ -1,6 +1,22 @@
+import { IsInt, IsPositive, IsNotEmpty, IsDate } from 'class-validator';
+
 export class CreateReservaDto {
-    UsuarioID: number;
-    EspacioID: number;
-    SesionID: number;
-    FechaReserva: Date;
-  }
+  @IsNotEmpty({ message: 'El UsuarioID es obligatorio' })
+  @IsInt({ message: 'El UsuarioID debe ser un número entero' })
+  @IsPositive({ message: 'El UsuarioID debe ser un número positivo' })
+  UsuarioID: number;
+
+  @IsNotEmpty({ message: 'El EspacioID es obligatorio' })
+  @IsInt({ message: 'El EspacioID debe ser un número entero' })
+  @IsPositive({ message: 'El EspacioID debe ser un número positivo' })
+  EspacioID: number;
+
+  @IsNotEmpty({ message: 'El SesionID es obligatorio' })
+  @IsInt({ message: 'El SesionID debe ser un número entero' })
+  @IsPositive({ message: 'El SesionID debe ser un número positivo' })
+  SesionID: number;
+
+  @IsNotEmpty({ message: 'La FechaReserva es obligatoria' })
+  @IsDate({ message: 'La FechaReserva debe ser una instancia válida de Date' })
+  FechaReserva: Date;
+}
